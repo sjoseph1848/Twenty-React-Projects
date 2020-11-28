38981554441e41daff8fdaa145f1248e141626d7
sjoseph1848 <sjoseph1848@gmail.com>
2020-11-28 16:38:31 -0600

    completed dayone project

--- day-one/src/Form.js
+++ day-one/src/Form.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+    let error = password === password2 ? true : false;
+
+    const onSubmit = e => {
+        e.preventDefault();
+        
+        console.log(error);
+        console.log(password);
+        console.log(password2);
+    }
+
+
   return (
     <div className='container'>
-      <form action='' className='form'>
+      <form onSubmit={onSubmit} className='form'>
         <h2>Sign Up</h2>
         <div className="form-control">
-          <label for="username">Username</label>
-          <input type="text" id="username" placeholder="Enter username" />
+          <label htmlFor="username">Username</label>
+          <input onChange={(e) => setUsername(e.target.value)} type="text" id="username" placeholder="Enter username" />
           <small>Error message</small>
         </div>
         <div className="form-control">
-          <label for="email">Email</label>
-          <input type="text" id="email" placeholder="Enter email" />
+          <label htmlFor="password">Password</label>
+          <input onChange={(e) => setPassword(e.target.value)} type="password" id="password" placeholder="Enter password" />
           <small>Error message</small>
         </div>
-        <div className="form-control">
-          <label for="password">Password</label>
-          <input type="password" id="password" placeholder="Enter password" />
-          <small>Error message</small>
-        </div>
-        <div className="form-control">
-          <label for="password2">Confirm Password</label>
+        <div className={error ? "form-control": "form-control error"}>
+          <label htmlFor="password2">Confirm Password</label>
           <input
+            onChange={(e) => setPassword2(e.target.value)}
             type="password"
             id="password2"
             placeholder="Enter password again"
           />
           <small>Error message</small>
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={!error}>Submit</button>
       </form>
     </div>
   );
