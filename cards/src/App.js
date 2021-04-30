8039f8d27c501f8ea9cabb7c6c473846a3710876
sjoseph1848 <sjoseph1848@gmail.com>
2021-04-29 22:29:45 -0500

    updated the react cards

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,22 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './App.css';
-import City from './City';
 import Austin from './imgs/Austin.jpg';
 import LA from './imgs/LA.jpg';
 import Chi from './imgs/Chi.jpg';
 import Denver from './imgs/Denver.jpg';
 import STL from './imgs/STL.jpg';
-import { act } from 'react-dom/test-utils';
-
 
 function App() {
+  const panels = document.querySelectorAll('.panel');
+
+  panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+      removeActiveClasses();
+      panel.classList.add('active');
+    });
+  });
+
+  function removeActiveClasses() {
+    panels.forEach((panel) => {
+      panel.classList.remove('active');
+    });
+  }
+
   return (
-    <div className="container">
-        <City img={Austin} loc="Austin"/>      
-        <City img={LA} loc="Los Angeles"/>      
-        <City img={Chi} loc="Chicago"/>      
-        <City img={Denver} loc="Denver"/>      
-        <City img={STL} loc="St. Louis"/>      
+    <div className='container'>
+      <div className='panel' style={{ backgroundImage: `url(${Austin})` }}>
+        <h3>Austin</h3>
+      </div>
+      <div className='panel' style={{ backgroundImage: `url(${LA})` }}>
+        <h3>Los Angeles</h3>
+      </div>
+      <div className='panel' style={{ backgroundImage: `url(${Chi})` }}>
+        <h3>Chicago</h3>
+      </div>
+      <div className='panel' style={{ backgroundImage: `url(${Denver})` }}>
+        <h3>Denver</h3>
+      </div>
+      <div className='panel' style={{ backgroundImage: `url(${STL})` }}>
+        <h3>St. Louis</h3>
+      </div>
     </div>
   );
 }
